e6f4e4f7e311d176e107908b4971c0751c4020bf
Xavier Perez <perezxavier918@gmail.com>
2021-09-21 20:32:38 -0600

    Agregada tarea 2

---
+++ Tarea ejercicio 1/Clientes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_ejercicio_1
+{
+    class Clientes
+    {
+        private string persona;
+        public string Persona
+        {
+            get { return persona; }
+            set { persona = value; }
+        }
+
+        private string cuenta;
+        public string Cuenta
+        {
+            get { return cuenta; }
+            set { cuenta = value; }
+        }
+    }
+}
---
+++ Tarea ejercicio 2/Tarea ejercicio 2/Alumno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_ejercicio_2
+{
+    class Alumno
+    {
+        string carnet;
+        public string Carnet
+        {
+            get { return carnet; }
+            set { carnet = value; }
+        }
+        string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        string apellido;
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        string materia;
+        public string Materia
+        {
+            get { return materia; }
+            set { materia = value; }
+        }
+        float[] calificaciones = new float[3];
+        public float[] Calificaciones
+        {
+            get { return calificaciones; }
+            set { calificaciones = value; }
+        }
+    }
+}
